b42d18e782154c4201f53d664a1ae64d4f3c3ac9
Giovani Farias <riccog.25@gmail.com>
2021-06-27 13:56:03 -0300

    return current user train

--- lib/wabanex/users/get.ex
+++ lib/wabanex/users/get.ex
@@ -1,5 +1,7 @@
 defmodule Wabanex.Users.Get do
-  alias Wabanex.{Repo, User}
+  import Ecto.Query
+
+  alias Wabanex.{Repo, User, Training}
   alias Ecto.UUID
 
   def call(id) do
@@ -15,7 +17,15 @@ defmodule Wabanex.Users.Get do
   defp handle_response({:ok, uuid}) do
     case Repo.get(User, uuid) do
       nil -> {:error, "User not found"}
-      user -> {:ok, user}
+      user -> {:ok, load_training(user)}
     end
+  end
+
+  defp load_training(user) do
+    today = Date.utc_today()
+
+    query = from train in Training, where: ^today >= train.start_date and ^today <= train.end_date
+
+    Repo.preload(user, trainings: {first(query, :inserted_at), :exercises})
   end
 end
